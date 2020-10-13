e47e06cb0b537423f02ba366ecdd11a5162aada6
ArtemEvgl <Usbhawk5@gmail.com>
2020-10-13 23:38:36 +0300

    ShellSort and QuickSort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        
+        
+        public static void QuickSort(int[] items)
+        {
+            QuickSort(items, 0, items.Length - 1);
+        }
+
+        private static void QuickSort(int[] items, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = items[(left + right) / 2];
+            while (i<=j)
+            {
+                while (pivot > items[i])
+                {
+                    i++;
+                }
+                while (pivot < items[j])
+                {
+                    j--;
+                }
+                if (i<=j)
+                {
+                    int temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+            if (j > left)
+            {
+                QuickSort(items, left, j);
+            }
+            if (i < right)
+            {
+                QuickSort(items, i, right);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 4, 6, 5, 8 };
+
+            Console.Write("Массив до сортировки: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.Write(" \nОтсортированный массив: ");
+
+            QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ShellSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+        static void ShellSort(int[] array)
+        {
+            //расстояние между элементами, которые сравниваются
+            int d = array.Length / 2;
+            while(d >= 1) {
+                for (int i = d; i < array.Length; i++)
+                {
+                    int j = i;
+                    while (j >= d && array[j - d] > array[j])
+                    {
+                        Swap(ref array[j - d], ref array[j]);
+                        j = j - d;
+                    }
+                }
+                d = d / 2;
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { -2, -3, -1, 0, 12, 22, 103, 7, 4, 2, 6, 5, 8 };
+
+            ShellSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
